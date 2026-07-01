4565eca83b42d7eb5b911875c80e74695ff0c30a
KagurazakaNyaa <dev@kagurazakanyaa.com>
2026-07-01 08:42:10 +0800

    feat: Bevy Plugin scaffolding

---
+++ src/lib.rs
@@ -0,0 +1,8 @@
+use bevy::prelude::*;
+
+#[derive(Debug, Clone, Copy, Default)]
+pub struct PveSpawningModPlugin;
+
+impl Plugin for PveSpawningModPlugin {
+    fn build(&self, _app: &mut App) {}
+}
